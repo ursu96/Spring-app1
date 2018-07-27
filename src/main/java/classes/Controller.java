e74eed2f81b9197a382a35844885c0f0ad5c1197
ursu96 <ursualexandru96@gmail.com>
2018-07-27 23:55:32 +0300

    New File

--- src/main/java/classes/Controller.java
+++ src/main/java/classes/Controller.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.ws.WebEndpoint;
+import java.util.logging.Logger;
 
 @RestController
 public class Controller {
 
+    static Logger log = Logger.getLogger(MainApp.class.getName());
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String afiseaza(){
-        return "Hello World!";
-    }
+        String message = "Hello World!";
+        log.warning("Mesajul este:" + message);
+        return message;
 
+    }
 }
