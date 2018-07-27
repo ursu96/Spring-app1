9d943b302225828e7acec56c46b3fbfa41665bbf
ursu96 <ursualexandru96@gmail.com>
2018-07-27 23:41:22 +0300

    first commit

---
+++ src/main/java/classes/Controller.java
@@ -0,0 +1,16 @@
+package classes;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.WebEndpoint;
+
+@RestController
+public class Controller {
+
+    @
+    public String afiseaza(){
+        return "salut gigele"
+    }
+
+}
