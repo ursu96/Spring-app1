56000203909397b41d302ae152365064cafec454
ursu96 <ursualexandru96@gmail.com>
2018-07-27 23:35:20 +0300

    New File

---
+++ src/main/java/classes/Aplicatia3.java
@@ -0,0 +1,11 @@
+package classes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplicatia3 {
+     public static void main(String[] args){
+         SpringApplication.run(Aplicatia3.class, args);
+     }
+}
